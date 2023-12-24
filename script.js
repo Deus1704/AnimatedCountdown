6b3f278b5dd7af33c916c026413a7f5ee3903075
Jayraj Dulange <117574289+Deus1704@users.noreply.github.com>
2023-12-24 09:52:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
   const countdownElement = document.getElementById("countdown");
   const finalAnimationsFolder = "./finalAnimations";
-  let countdown = 9;
+  let days = 5; //Change as required
+  let countdown = days * 24 * 60 * 60;
 
   function updateCountdown() {
-    const seconds = countdown;
+    const daysRemaining = Math.floor(countdown / (24 * 60 * 60));
+    const hoursRemaining = Math.floor((countdown % (24 * 60 * 60)) / 3600);
+    const minutesRemaining = Math.floor((countdown % 3600) / 60);
+    const secondsRemaining = countdown % 60;
+
+    const daysDigit2 = Math.floor((daysRemaining % 100) / 10);
+    const daysDigit3 = daysRemaining % 10;
 
     countdownElement.innerHTML = `
-      <div class="neon-number">${getNeonImage(seconds)}</div>
+      <div class="neon-number dd">${getNeonImage(daysDigit2)}</div>
+      <div class="neon-number dd">${getNeonImage(daysDigit3)}</div>
+
+      <div class="neon-number hr">${getNeonImage(Math.floor(hoursRemaining / 10))}</div>
+      <div class="neon-number hr">${getNeonImage(hoursRemaining % 10)}</div>
+
+      <div class="neon-number mm">${getNeonImage(Math.floor(minutesRemaining / 10))}</div>
+      <div class="neon-number mm">${getNeonImage(minutesRemaining % 10)}</div>
+
+      <div class="neon-number ss">${getNeonImage(Math.floor(secondsRemaining / 10))}</div>
+      <div class="neon-number ss">${getNeonImage(secondsRemaining % 10)}</div>
     `;
   }
 
